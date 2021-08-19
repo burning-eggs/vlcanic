68c38862780c11bba9a3c1f1f981240dbd0d15ea
wake-code <87005812+wake-code@users.noreply.github.com>
2021-08-19 11:45:41 +0300

    window setup

---
+++ vlcanic/vlc_window.cpp
@@ -0,0 +1,16 @@
+#include "vlc_window.hpp"
+
+namespace vlc {
+	VlcWindow::VlcWindow(int w, int h, std::string name) : width{ w }, height{ h }, windowName{ name } {
+		initWindow();
+	}
+
+	void VlcWindow::initWindow() {
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
+	}
+}
